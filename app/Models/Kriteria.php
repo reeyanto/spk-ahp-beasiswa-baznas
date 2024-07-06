3eed211ba3746f5b311411008487c75db2725688
Riyanto <riyanto.droider@gmail.com>
2024-07-06 16:29:56 +0700

    Menambahkan relasi antara kriteria dan sub-kriteria

--- app/Models/Kriteria.php
+++ app/Models/Kriteria.php
@@ -2,8 +2,9 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\SubKriteria;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Kriteria extends Model
 {
@@ -14,5 +15,9 @@ class Kriteria extends Model
 
     public function alternatif_kriteria() {
         return $this->hasMany(AlternatifKriteria::class);
+    }
+
+    public function subkriteria() {
+        return $this->hasMany(SubKriteria::class);
     }
 }
