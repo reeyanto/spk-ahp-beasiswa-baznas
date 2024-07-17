9f7cf7295704b4097cf0cab65f81dc4da5e71ed3
Riyanto <riyanto.droider@gmail.com>
2024-07-17 11:29:48 +0700

    Perbaikan lebar kolom tiap module

--- resources/views/admin/password/index.blade.php
+++ resources/views/admin/password/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <h1 class="h3 mb-3">Password</h1>
     <div class="row">
-        <div class="col-12 col-lg-8 col-xxl-12 d-flex">
+        <div class="col-12 col-lg-12 col-xxl-12 d-flex">
             <div class="card flex-fill">
                 <div class="card-header bg-danger">
                     <a href="{{ route('dashboard.index') }}" class="btn btn-sm btn-warning">
